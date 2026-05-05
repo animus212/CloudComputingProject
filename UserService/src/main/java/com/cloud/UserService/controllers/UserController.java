5c20bf2e0bb5e171c364c7073a12a076b43e8c8f
animus212 <mohanadhussein536@gmail.com>
2026-05-05 18:37:24 +0300

    added user update and delete

--- UserService/src/main/java/com/cloud/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/cloud/UserService/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.cloud.UserService.controllers;
 
+import com.cloud.UserService.dtos.requests.UserUpdateRequest;
 import com.cloud.UserService.dtos.responses.UserResponse;
 import com.cloud.UserService.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -27,5 +30,25 @@ public class UserController {
             @AuthenticationPrincipal UserDetails userDetails
     ) {
         return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or #userDetails.username == @userService.getUserById(#id).username")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUserById(
+            @PathVariable Long id,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        userService.deleteUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or #userDetails.username == @userService.getUserById(#id).username")
+    public ResponseEntity updateUserById(
+            @PathVariable Long id,
+            @AuthenticationPrincipal UserDetails userDetails,
+            @Valid @RequestBody UserUpdateRequest request
+            ) {
+       return ResponseEntity.ok( userService.updateUserById(id, request));
     }
 }
